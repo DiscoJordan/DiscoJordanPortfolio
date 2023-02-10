2cd25416561790a445c735ab008acc8c86e45f5e
Jordan <56343088+DiscoJordan@users.noreply.github.com>
2023-02-10 20:51:59 +0300

    3.2
    spoiler

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ $(document).ready(function (){
     $('.header_burger').click(function(event) {
         $('.header_burger, .nav').toggleClass('active');
     });
-
+    $('.spoiler__title').click(function(event) {
+        $('.spoiler__title').toggleClass('active').next().slideToggle(100);
+        $(".single_photos").css("display","flex");
+    });
 });
